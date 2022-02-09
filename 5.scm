df0f43217320a25271c53cd496b3f25921d215e1
Will Harris <will.t.harris@protonmail.com>
2022-02-09 09:46:26 -0800

    add solutions from start of ch. 5

---
+++ 5.scm
@@ -0,0 +1,126 @@
+#lang scheme
+(define (atom? x) (not (or (pair? x) (null? x))))
+
+
+; Removes an atom from any(?) list of S-expressions
+(define rember*
+  (lambda (a l)
+    (cond
+      ((null? l) '())
+      ((atom? (car l))
+       (cond
+         ((eq? (car l) a) (rember* a (cdr l)))
+         (else (cons (car l) (rember* a (cdr l))))))
+      (else (cons (rember* a (car l)) (rember* a (cdr l)))))))
+
+; occur* test
+(rember* 'cup '((coffee) cup ((tea) cup) (and (hick)) cup)) "should equal" '((coffee) ((tea)) (and (hick))) (newline)
+
+
+; Inserts the atom `new` to the right of `old` anywhere in a list of S-expressions
+(define insertR*
+  (lambda (new old l)
+    (cond
+      ((null? l) '())
+      ((atom? (car l))
+       (cond
+         ((eq? (car l) old) (cons old (cons new (insertR* new old (cdr l)))))
+         (else (cons (car l) (insertR* new old (cdr l))))))
+      (else (cons (insertR* new old (car l)) (insertR* new old (cdr l)))))))
+
+; insertR* test
+(insertR* 'roast 'chuck '((how much (wood) could ((a (wood) chuck)) (((chuck))) (if (a) ((wood chuck))) could chuck wood)))
+"should equal"
+'((how much (wood) could ((a (wood) chuck roast)) (((chuck roast))) (if (a) ((wood chuck roast))) could chuck roast wood)) (newline)
+
+
+; Returns the number of times an atom exists in a list of S-expressions
+(define occur*
+  (lambda (a l)
+    (cond
+      ((null? l) 0)
+      ((atom? (car l))
+       (cond
+         ((eq? (car l) a) (add1 (occur* a (cdr l))))
+         (else (occur* a (cdr l)))))
+      (else (+ (occur* a (car l)) (occur* a (cdr l)))))))
+         
+; occur* test
+(occur* 'banana '((banana) (split ((((banana ice))) (cream (banana)) sherbet)) (banana) (bread) (banana brandy))) "should equal" 5 (newline)
+
+
+; Replaces all instances of `old` with `new` in a list of S-expressions
+(define subst*
+  (lambda (new old l)
+    (cond
+      ((null? l) '())
+      ((atom? (car l))
+       (cond
+         ((eq? (car l) old)
+          (cons
+           new
+           (subst* new old (cdr l))))
+         (else
+          (cons
+           (car l)
+           (subst* new old (cdr l))))))
+      (else
+       (cons
+        (subst* new old (car l))
+        (subst* new old (cdr l)))))))
+
+; subst* test
+(subst* 'orange 'banana '((banana) (split ((((banana ice))) (cream (banana)) sherbet)) (banana) (bread) (banana brandy)))
+"should equal"
+'((orange) (split ((((orange ice))) (cream (orange)) sherbet)) (orange) (bread) (orange brandy)) (newline)
+
+
+; Inserts atom `new` to the left of `old` anywhere in a list of S-expressions
+(define insertL*
+  (lambda (new old l)
+    (cond
+      ((null? l) '())
+      ((atom? (car l))
+       (cond
+         ((eq? (car l) old)
+          (cons new
+                (cons old
+                      (insertL* new old (cdr l)))))
+         (else
+          (cons (car l)
+                (insertL* new old (cdr l))))))
+      (else
+       (cons (insertL* new old (car l))
+             (insertL* new old (cdr l)))))))
+
+; insertL* test
+(insertL* 'pecker 'chuck '((how much (wood) could ((a (wood) chuck)) (((chuck))) (if (a) ((wood chuck))) could chuck wood)))
+"should equal"
+'((how much (wood) could ((a (wood) pecker chuck)) (((pecker chuck))) (if (a) ((wood pecker chuck))) could pecker chuck wood)) (newline)
+
+
+; Returns true if the atom exists in the list of S-expressions
+(define member*
+  (lambda (a l)
+    (cond
+      ((null? l) #f)
+      ((atom? (car l))
+       (or (eq? (car l) a) (member* a (cdr l))))
+      (else
+       (or (member* a (car l)) (member* a (cdr l)))))))
+         
+; member* tests
+(member* 'chips '((potato) (chips ((with) fish) (chips)))) "should equal" #t (newline)
+(member* 'boop '((potato) (chips ((with) fish) (chips)))) "should equal" #f (newline)
+
+
+; Finds the leftmost atom in a non-empty list of S-expressions that doesn't contain the empty list
+(define leftmost
+  (lambda (l)
+    (cond
+      ((atom? (car l)) (car l))
+      (else (leftmost (car l))))))
+
+; leftmost tests
+(leftmost '((potato) (chips ((with) fish) (chips)))) "should equal" 'potato (newline)
+(leftmost '(((hot) (tuna (and))) cheese)) "should equal" 'hot (newline)
